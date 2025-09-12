855eea08b2fbc4c71b6fc38603693634fbe7f01b
Anett Moor <anemoo@taltech.ee>
2025-09-12 12:03:16 +0300

    vjdij

---
+++ hyper-connectx/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Utils;
+
+Console.WriteLine("Hello, World!");
+
+// div 3 = fizz
+// div 5 = buzz
+// div 3 & 5 = fizzbuzz
+
+// C# cant change variable type after variable was created
+
+
+FizzBuzzLogic.OutputFizzBuzzInstructions();
+var countTo = FizzBuzzLogic.FizzBuzzCount();
+FizzBuzzLogic.OutputFizzBuzz(countTo);
+
+
+
+
+
+    
---
+++ tic-tac-toe/MenuSystem/Menu.cs
@@ -0,0 +1,54 @@
+﻿namespace MenuSystem;
+
+public class Menu
+{
+    public List<MenuItem> MenuItems { get; set; } = [];
+
+
+    public void AddMenuItems(List<MenuItem> items)
+    {
+        // check for validity
+        // add 
+        foreach (var item in items)
+        {
+            // control check - dictionary
+            
+            
+            
+            MenuItems.Add(item);
+        }
+    }
+    
+    public void Run()
+    {
+        var menuIsDone = false;
+        do
+        {
+            //  display menu
+            DisplayMenu();
+            
+            // get user input
+            Console.Write("Please make a slection: ");
+            var userInput = Console.ReadLine();
+            // validate input
+            // execute choice
+            
+            if(userInput == "X")
+            {
+                menuIsDone = true;
+            }
+            
+        } while (!menuIsDone);
+    }
+    
+        // loop back to top
+        
+        
+        
+        private void DisplayMenu()
+        {
+            foreach (var item in MenuItems)
+                Console.WriteLine(item.Label);
+        }
+        
+}
