70231f8d92a10079a9881237f34f57ae30d5d84e
Anett Moor <anemoo@taltech.ee>
2025-10-26 14:30:34 +0200

    .

---
+++ tic-tac-toe/BLL/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace BLL;
+
+public abstract class BaseEntity
+{
+    // creating code side id's
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
---
+++ tic-tac-toe/DAL/appDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BLL;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<GameConfiguration> GameConfigurations { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        //remove all cascade deletion
+        foreach (var relationship in modelBuilder.Model
+                     .GetEntityTypes()
+                     .Where(e => !e.IsOwned())
+                     .SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.Restrict;
+        }
+    }
+}
